a486a267c3342d801c26baa07066ff2333c453a6
oLNidfwworld <kuvetov@bk.ru>
2024-11-29 16:35:14 +0300

    update sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-which pnpm
-which pm2
-
-export PATH=/root/.local/share/pnpm:/root/.nvm/versions/node/v22.11.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH=/root/.local/share/pnpm:/root/.nvm/versions/node/v22.11.0/bin
 
 npx pnpm install --frozen-lockfile
 npx pnpm run build
